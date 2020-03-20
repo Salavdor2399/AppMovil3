90279d65f8c6588af4f5bcad99f1326af312d234
Jose salvador medina olguin <61695656+Salavdor2399@users.noreply.github.com>
2020-03-20 08:41:00 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, StyleSheet, Alert, TextInput,Button,} from 'react-native';
+import { Container,Text, Header, Content, Card, CardItem,Body,Item, Label, Input,Icon} from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Registro from './src/Registro';
+import Login from './src/Login';
+import Principal from './src/Principal';
+import Indicator from './src/Indicator';
+import Planetas from './src/films';
+
+const Stack = createStackNavigator();
+
+const App:  () => React$Node = () =>{
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+        name="Login" 
+        component={Login} />
+        <Stack.Screen 
+        name="Films" 
+        component={Films} />
+        <Stack.Screen 
+        name="Registro" 
+        component={Registro} />
+         <Stack.Screen 
+        name="Principal" 
+        component={Principal} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+//JOSE SALVADOR MEDINA OLGUIN
---
+++ src/Indicator.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {ActivityIndicator,StyleSheet,Text,View,Modal} from 'react-native';
+
+function Indicator ({isVisible}) {
+   
+    return (
+    <Modal
+    visible={isVisible}   
+    >
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size="large" color="#ffffff" />
+      </View>
+    </Modal>
+    )
+  }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 15
+  }
+});
+export default Indicator
+
+//JOSE SALVADOR MEDINA OLGUIN
